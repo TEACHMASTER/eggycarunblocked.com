594f531b43220f4e8aa618613bd029a7f0713808
TEACHMASTER <1577387003@qq.com>
2024-10-21 22:18:33 +0800

    add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-B4P7XCJTG6"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-B4P7XCJTG6');
+        `}
+      </Script>
     </Layout>
   );
 }
